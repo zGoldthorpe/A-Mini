ccf14a014a83f319b166226d2993ffd3c992e7ab
Zach Goldthorpe <zgoldtho@ualberta.ca>
2023-10-22 17:06:22 -0600

    Improved `@ensure_types` wrapper error message
    Types can be unified via a tuple, but this was not reflected by the
    `Syntax` class (error message threw error messages)

--- ampy/ensuretypes.py
+++ ampy/ensuretypes.py
@@ -8,6 +8,11 @@ class Syntax:
         self._allow_undef_kwargs = True
         self._allow_extra_kwargs = False
 
+    def _type_name(ty):
+        if isinstance(ty, type):
+            return ty.__name__
+        if isinstance(ty, tuple):
+            return f"({', '.join(Syntax._type_name(t) for t in ty)})"
     def extract_syntax(*args, **kwargs):
         return Syntax(*[type(arg) for arg in args], **{kw:type(kwargs[kw]) for kw in kwargs})
 
@@ -24,14 +29,14 @@ class Syntax:
             raise TypeError(f"{func_name} expects {len(self._types)} positional arguments; {len(args)} given.")
         for i, (arg, ty) in enumerate(zip(args, self._types)):
             if not isinstance(arg, ty):
-                raise TypeError(f"{func_name} argument {i} expects {ty.__name__}; {type(arg).__name__} given.")
+                raise TypeError(f"{func_name} argument {i} expects {Syntax._type_name(ty)}; {Syntax._type_name(type(arg))} given.")
         for kw in kwargs:
             if kw not in self._kwtypes:
                 if self._allow_extra_kwargs:
                     continue
                 raise TypeError(f"{func_name} received unexpected keyword argument {kw}.")
             if not isinstance(kwargs[kw], self._kwtypes[kw]):
-                raise TypeError(f"{func_name} keyword argument {kw} expects {self._kwtypes[kw].__name__}; {type(kwargs[kw]).__name__} given.")
+                raise TypeError(f"{func_name} keyword argument {kw} expects {Syntax._type_name(self._kwtypes[kw])}; {Syntax._type_name(type(kwargs[kw]))} given.")
         if not self._allow_undef_kwargs:
             for kw in self._kwtypes:
                 if kw not in kwargs:
@@ -39,7 +44,7 @@ class Syntax:
 
     def __repr__(self):
         positional = ", ".join(ty.__name__ for ty in self._types)
-        keyword = ", ".join(f"{kw}:{self._kwtypes[kw].__name__}" for kw in self._kwtypes)
+        keyword = ", ".join(f"{kw}:{Syntax._type_name(self._kwtypes[kw])}" for kw in self._kwtypes)
         if not positional and not keyword:
             return "Syntax()"
         if not positional:
@@ -90,7 +95,7 @@ def ensure_multi(*syntaxes):
                     syntax.check(func.__name__, *args, **kwargs)
                     return func(*args, **kwargs)
             # if this line is reached, then none of the clues matched
-            raise TypeError(f"""{func.__name__} received unrecognised argument pattern
+            raise TypeError(f"""{Syntax._type_name(func)} received unrecognised argument pattern
 \t{repr(Syntax.extract_syntax(*args, **kwargs))}
 Valid syntaxes are:
 \t{(chr(10)+chr(9)).join(repr(syntax) for _, syntax in syntaxes)}""")
