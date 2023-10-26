046861d62cc47e1a7c256fdbd68b45ddafff1eda
Zach Goldthorpe <zgoldtho@ualberta.ca>
2023-10-25 21:40:50 -0600

    Added more tests for syntax parsing
    ... though perhaps this is unnecessary since all code from here on
    will be using `Syntax` wrappers, and thus testing will be continuous.

--- tests/ensuretypes.py
+++ tests/ensuretypes.py
@@ -1,17 +1,114 @@
 import sys
 
 from ampy.ensuretypes import Syntax
+from ampy.printing import tame_whitespace as tw
 from tests.tools import PythonExecutionTestSuite
 
 ts = PythonExecutionTestSuite("ensure-types")
 
-ts.exec("@Syntax(int)\ndef foo(x): return x",
+ts.exec(tw("""
+        @Syntax(int)
+        def foo(x):
+            return x"""),
         "foo(0)",
-        "try:\n\tfoo('a')\nexcept TypeError:\n\tsafe=True",
-        "assert safe == True",
+        ("foo('a')", TypeError),
         state=dict(Syntax=Syntax))
 
-#TODO more tests
+ts.exec(tw("""
+        @Syntax((int, float))
+        def foo(x): return x"""),
+        "foo(0)",
+        "foo(0.)",
+        ("foo('a')", TypeError),
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @Syntax([list, int, [2, 5]])
+        def foo(ls):
+            s = 0
+            for i in range(len(ls)):
+                s += ls[i]
+            return s"""),
+        "foo([1, 2])",
+        "foo([1,2,3,4,5])",
+        ("foo([1])", TypeError),
+        ("foo([1,2,3,4,5,6])", TypeError),
+        ("foo(0)", TypeError),
+        ("foo([1., 2, 3])", TypeError),
+        ("foo([1, 2, 3.])", TypeError),
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @Syntax([int])
+        def foo(ls):
+            m = 0
+            for x in ls:
+                m = x if m < x else m
+            return m"""),
+        "foo([])",
+        "foo([0])",
+        "foo([0]*1000)",
+        ("foo(0)", TypeError),
+        ("foo([1, 2, 3.])", TypeError),
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @Syntax([str, [..., 3]])
+        def foo(ls):
+            m = 0
+            for x in ls:
+                y = len(x)
+                m = y if m < y else m
+            return m"""),
+        "foo([])",
+        "foo(['a'])",
+        "foo(['a', 'b', 'c'])",
+        "foo('abc')", # bug, or feature?
+        ("foo(0)", TypeError),
+        ("foo(['a', 'b', 'c', 'd'])", TypeError),
+        ("foo(['a', 'b', 0])", TypeError),
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @Syntax(int).returns([set, int])
+        def foo(x): return {x, x**x}"""),
+        "assert foo(3) == {3, 27}",
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @(Syntax({int}) >> int)
+        def foo(gen): return sum(gen)"""),
+        "assert foo(i for i in range(5)) == 10",
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @(Syntax(int) >> {int})
+        def foo(N):
+            for i in range(N):
+                yield i"""),
+        "assert list(foo(3)) == [0, 1, 2]",
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @(Syntax(int) >> {int})
+        def bad(N):
+            for i in range(N):
+                yield i
+            yield 'hello'"""),
+        ("list(bad(3))", TypeError),
+        state=dict(Syntax=Syntax))
+
+ts.exec(tw("""
+        @Syntax({str:int}, int).returns(int)
+        def foo(dc, N):
+            n = 0
+            for k in dc:
+                if int(dc[k]) < N:
+                    n += 1
+            return n"""),
+        "assert foo(dict(a=3, b=5, c=10), 8) == 2",
+        ("foo(dict(a=3, b=5, c='10'), 8)", TypeError),
+        state=dict(Syntax=Syntax))
 
 if __name__ == "__main__":
     ts.print_results()
